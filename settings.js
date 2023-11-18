08a1151c5aa73a1b65d77fa64628c11227a742dc
Ilmitttr <147111356+Ilmitttr@users.noreply.github.com>
2023-11-18 16:36:21 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,10 +1,10 @@
 const fs = require('fs')
 
-global.creator = 'Zeeoneofc'// yourname
-global.MONGO_DB_URI = "" //database mongodb 
+global.creator = 'IlmiXyz'// yourname
+global.MONGO_DB_URI = "mongodb+srv://gmingilmi:ilmi1234@cluster0.o712ees.mongodb.net/?retryWrites=true&w=majority" //database mongodb 
 global.ACTIVATION_TOKEN_SECRET = "-@Pqnap+@(/1jAPPnew/@10" //isi apa aja bebas
-global.your_email = "" //email
-global.email_password = "" //application password email
+global.your_email = "gmingilmi@gmail.com" //email
+global.email_password = "ilmi1234" //application password email
 global.limitCount = 10000
 global.YUOR_PORT = 8000
 global.loghandler = {
